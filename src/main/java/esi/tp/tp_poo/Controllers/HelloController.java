cfc2472423c9644b133d92f96b69130f27327e06
Mohamed <mm_maraf@esi.dz>
2024-05-19 18:57:52 +0100

    initial commit

---
+++ src/main/java/esi/tp/tp_poo/Controllers/HelloController.java
@@ -0,0 +1,14 @@
+package esi.tp.tp_poo.Controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module esi.tp.tp_poo {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens esi.tp.tp_poo to javafx.fxml;
+    exports esi.tp.tp_poo;
+    exports esi.tp.tp_poo.Controllers;
+    opens esi.tp.tp_poo.Controllers to javafx.fxml;
+}
