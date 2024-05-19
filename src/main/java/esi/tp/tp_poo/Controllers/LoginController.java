e250519045a1e2613803099b40d80c189b61c9bd
Mohamed <mm_maraf@esi.dz>
2024-05-19 19:36:02 +0100

    kkk

---
+++ src/main/java/esi/tp/tp_poo/Controllers/LoginController.java
@@ -0,0 +1,15 @@
+package esi.tp.tp_poo.Controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+
+public class LoginController {
+    @FXML
+    private Label labelll;
+
+
+    public void onClickutton(){
+        labelll.setText("hhhhhhhhhhhhhh");
+    }
+}
